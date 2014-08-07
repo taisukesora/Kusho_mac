5869de995a9528cf9a2fda5ad079dfe4d7a16689
taisukesora <nagasuyusuke@gmail.com>
2014-08-07 12:01:10 +0900

    lefthand & righthand & erase
    lefthand & righthand & erase

---
+++ Meshplay_open/Assets/Scripts/CreatePolygonMesh.cs
@@ -0,0 +1,231 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CreatePolygonMesh : MonoBehaviour {
+	private Mesh mesh;
+	// Use this for initialization
+	Queue<Vector3> path = new Queue<Vector3>();
+	Queue<Vector3> newVertices = new Queue<Vector3>();
+	Queue<Vector2> newUV = new Queue<Vector2>();
+	Queue<int> newTriangles = new Queue<int>();
+	const int sections = 12;
+	//vectors
+	Vector3[] basevec;
+	
+	//
+	int count = 0;
+	public bool mousePointed = false;
+	
+	//
+	public GameObject RightHand;
+	private Vector3 pos, newpos;
+	public float delta_distance;
+	public float weight2;
+	public float noise_factor = 1.0f;
+	
+	//timer
+	private float timer;
+	private float waitingTime=0.01f;
+	public bool drawing;
+
+	CubemanController script_cubeman;
+	bool Rhand_opening;
+	
+	void Start () {
+		script_cubeman = GameObject.Find ("Cubeman").GetComponent<CubemanController>();
+
+		basevec = new Vector3[sections];
+		mesh = new Mesh();    
+		//初期path
+		path.Enqueue(RightHand.transform.position);
+
+		//点の設定
+		for(int i=0;i<sections;i++){
+			basevec[i] = new Vector3(0.0f, Mathf.Cos (2*Mathf.PI*i/sections), Mathf.Sin(2*Mathf.PI*i/sections));
+		}
+
+		for(int i=0;i<sections;i++)
+		{
+			newVertices.Enqueue(path.Peek() + basevec[i]);
+			newUV.Enqueue(new Vector2(i/sections, 0.0f));
+		}
+		
+		//Triangles
+		/*
+    newTriangles.Enqueue(1);newTriangles.Enqueue(2);newTriangles.Enqueue(0);
+    newTriangles.Enqueue(2);newTriangles.Enqueue(3);newTriangles.Enqueue(0);
+    newTriangles.Enqueue(3);newTriangles.Enqueue(4);newTriangles.Enqueue(0);
+    */
+		
+		mesh.vertices = newVertices.ToArray();
+		mesh.uv = newUV.ToArray();
+		mesh.triangles = newTriangles.ToArray();
+		
+		mesh.RecalculateNormals();
+		mesh.RecalculateBounds();
+		
+		GetComponent<MeshFilter>().sharedMesh = mesh;
+		GetComponent<MeshFilter>().sharedMesh.name = "myMesh";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		/*
+    //点をウニョウニョ動かす、生き物っぽい
+    float k = 0.01f;
+    Vector3[] tmp = newVertices.ToArray();
+    for(int i=0;i<tmp.Length;i++)
+      {
+	tmp[i].y += k*Mathf.Sin(Time.time+5*i);
+	tmp[i].z += k*Mathf.Cos(Time.time+4*i);
+      }
+    newVertices.Clear();
+    newVertices = new Queue<Vector3>(tmp);
+	*/
+		
+		//delta_distance = 0.0f;
+		
+		drawing = false;
+		timer += Time.deltaTime;
+		if(timer>waitingTime)
+		{
+			//アクティブでない場合
+			if(!RightHand.activeSelf)
+			{
+				return;
+			}
+			//if(script_cubeman.isRHandopen()==1){
+				newpos = RightHand.transform.position;
+				//距離が短すぎる場合
+				delta_distance = Vector3.Distance(pos, newpos);
+				Quaternion rot = Quaternion.FromToRotation(new Vector3(1.0f, 0.0f, 0.0f), newpos - pos);
+				pos = newpos;
+				newpos += noise_factor* (new Vector3(0.0f, 0.0f, Mathf.PerlinNoise (RightHand.transform.position.x, RightHand.transform.position.y)));
+
+
+				if(delta_distance < 0.1f)
+				{
+					return;
+				}
+				//アクティブな場合
+				timer = 0;
+
+				if(delta_distance > 0.3f){
+					drawing = true;
+				}
+				
+				//Quaternion rot = Quaternion.FromToRotation(new Vector3(1.0f, 0.0f, 0.0f), newpos - pos);
+				int pathcount = path.Count-1;
+				path.Enqueue(newpos);
+				
+				//点の設定
+				int vertcount = newVertices.Count-1;
+				weight2 = Mathf.Exp (-delta_distance*5);
+				//Debug.Log("weight ,delta_distance ="+ weight2); 
+				for(int i=0;i<sections;i++)
+				{
+					newVertices.Enqueue(rot*(weight2*basevec[i]) + newpos);
+					newUV.Enqueue(new Vector2(i/sections, 1.0f));
+				}
+				
+				//UV
+				
+				//Triangles
+				int tricount = newTriangles.Count-1;
+				//Debug.Log("triangle"+tricount);
+				if(path.Count<=1000)
+				{	  
+					//手が閉じている場合
+					if(script_cubeman.isRHandopen()==2){
+						//2連続で開いていると判定された場合
+						if(Rhand_opening){
+							for(int i = 0;i<sections*3;i++){
+								newTriangles.Enqueue(0);
+							}
+						}
+						else{
+							//初めての場合は閉じているときと同じように動作
+							Rhand_opening = true;
+							int indicesFrom = vertcount - (sections) + 1;
+							for(int i = 0; i < sections-1; i++){
+								newTriangles.Enqueue(indicesFrom + i);
+								newTriangles.Enqueue(indicesFrom + (sections) + i);
+								newTriangles.Enqueue(indicesFrom + (i+1));
+								
+								newTriangles.Enqueue(indicesFrom + (sections) + (i));
+								newTriangles.Enqueue(indicesFrom + (sections) + (i+1));
+								newTriangles.Enqueue(indicesFrom + (i+1));
+								
+							}
+							newTriangles.Enqueue(indicesFrom + sections -1);
+							newTriangles.Enqueue (indicesFrom + (sections)*2 -1);
+							newTriangles.Enqueue(indicesFrom);
+							
+							newTriangles.Enqueue(indicesFrom + sections*2 -1);
+							newTriangles.Enqueue (indicesFrom + (sections));
+							newTriangles.Enqueue(indicesFrom);
+						}
+					}
+					else{
+						//手が開いている場合
+						int indicesFrom = vertcount - (sections) + 1;
+						for(int i = 0; i < sections-1; i++){
+							newTriangles.Enqueue(indicesFrom + i);
+							newTriangles.Enqueue(indicesFrom + (sections) + i);
+							newTriangles.Enqueue(indicesFrom + (i+1));
+							
+							newTriangles.Enqueue(indicesFrom + (sections) + (i));
+							newTriangles.Enqueue(indicesFrom + (sections) + (i+1));
+							newTriangles.Enqueue(indicesFrom + (i+1));
+							
+						}
+						newTriangles.Enqueue(indicesFrom + sections -1);
+						newTriangles.Enqueue (indicesFrom + (sections)*2 -1);
+						newTriangles.Enqueue(indicesFrom);
+						
+						newTriangles.Enqueue(indicesFrom + sections*2 -1);
+						newTriangles.Enqueue (indicesFrom + (sections));
+						newTriangles.Enqueue(indicesFrom);
+					Rhand_opening = false;
+				}
+			}
+				else
+				{
+					//順にデキュー
+					path.Dequeue();
+					for(int i=0;i<sections;i++)
+					{
+						newVertices.Dequeue();
+						newUV.Dequeue();
+						newTriangles.Dequeue();newTriangles.Dequeue();newTriangles.Dequeue();
+						newTriangles.Dequeue();newTriangles.Dequeue();newTriangles.Dequeue();
+					}
+				}
+			//}
+			mesh.Clear();
+			mesh.vertices = newVertices.ToArray();
+			mesh.uv = newUV.ToArray();
+			mesh.triangles = newTriangles.ToArray();
+			mesh.RecalculateNormals();
+			mesh.RecalculateBounds();
+		}
+		count++;
+		
+		
+	}
+	
+	public void resetMesh()
+	{
+		mesh.Clear();
+		path.Clear();path.TrimExcess();
+		newVertices.Clear();newVertices.TrimExcess();
+		newUV.Clear();newUV.TrimExcess();
+		newTriangles.Clear();newTriangles.TrimExcess();
+		
+		Start();
+	}
+	
+	
+}
---
+++ Meshplay_open/Assets/Scripts/SimpleMeshRenderer.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+[RequireComponent( typeof( MeshRenderer ) )]
+[RequireComponent( typeof( MeshFilter ) )]
+
+public class SimpleMeshRenderer : MonoBehaviour {
+  Mesh mesh;
+  MeshFilter meshFilter;
+  
+  Vector3[] vertices;
+  int[] triangles;
+  Vector2[] uvs;
+  
+  int meshlength = 3*10;
+  
+  // Use this for initialization
+  void Start () {
+    mesh = new Mesh();
+    meshFilter = (MeshFilter)GetComponent("MeshFilter");
+  }
+  
+  // Update is called once per frame
+  void Update () {
+    mesh.Clear();
+    
+    vertices = new Vector3[3];
+    vertices[0] = Vector3.zero;
+    vertices[1] = new Vector3( 0, 0, 1 );
+    vertices[2] = new Vector3( 1, 0, 0 );
+		
+    triangles = new int[3];
+    triangles[0] = 0;
+    triangles[1] = 1;
+    triangles[2] = 2;
+    
+    uvs = new Vector2[3];
+    uvs[0] = Vector2.zero;
+    uvs[1] = Vector2.right;
+    uvs[2] = Vector2.up;
+    
+    mesh.vertices = vertices;
+    mesh.triangles = triangles;
+    mesh.uv = uvs;
+    
+    mesh.RecalculateNormals();
+    mesh.RecalculateBounds();
+    mesh.Optimize();
+    
+    meshFilter.sharedMesh = mesh;
+    meshFilter.sharedMesh.name = "SimpleMesh";
+  }
+}
